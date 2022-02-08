deaff5bfefa53462b2bbf0b1eb991a6c521de872
Hannu Ylitalo <hannu@ylitalo.eu>
2022-02-08 23:18:39 +0200

    Add Timeseries for the UI.

---
+++ frontend/quasar/src/pages/TimeSeries.vue
@@ -0,0 +1,20 @@
+<template>
+    <LogTimeSeries />
+</template>
+
+<script>
+
+import LogTimeSeries from 'components/LogTimeSeries.vue'
+
+export default {
+    components: {
+        LogTimeSeries
+    },
+
+    setup() {
+        return {
+            LogTimeSeries
+        }
+    },
+}
+</script>
